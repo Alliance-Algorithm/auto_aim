9b80d5117947eadacf15af22e488b1c98bfe5511
7emotions <paradise_c@qq.com>
2024-08-03 05:49:50 +0000

    Debug buff

--- src/auto_aim_controller.cpp
+++ src/auto_aim_controller.cpp
@@ -137,7 +137,8 @@ void Controller::gimbal_process() {
                     auto center = armors[0].center();
                     *ui_target_ = std::make_pair(center.x, center.y);
                 }
-                auto armor3d = ArmorPnPSolver::SolveAll(armors, tf, fx_, fy_, cx_, cy_, k1_, k2_, k3_);
+                auto armor3d =
+                    ArmorPnPSolver::SolveAll(armors, tf, fx_, fy_, cx_, cy_, k1_, k2_, k3_);
 
                 if (!armor3d.empty()) {
                     for (auto& object : armor3d) {
@@ -159,7 +160,8 @@ void Controller::gimbal_process() {
                     if (buff) {
                         RCLCPP_INFO(get_logger(), "Buff Detected!");
                     }
-                    if (auto buff3d = BuffPnPSolver::Solve(*buff, tf, fx_, fy_, cx_, cy_, k1_, k2_, k3_)) {
+                    if (auto buff3d =
+                            BuffPnPSolver::Solve(*buff, tf, fx_, fy_, cx_, cy_, k1_, k2_, k3_)) {
                         if (auto target = buff_tracker.Update(*buff3d, timestamp)) {
                             timestamp_ = timestamp;
                             target_.swap(target);
@@ -171,8 +173,8 @@ void Controller::gimbal_process() {
             }
         } while (false);
         counter++;
-        if (record_mode_ && (debug_mode_ || *stage_ == rmcs_msgs::GameStage::STARTED) && recorder_.is_opened()
-            && !img.empty()) {
+        if (record_mode_ && (debug_mode_ || *stage_ == rmcs_msgs::GameStage::STARTED)
+            && recorder_.is_opened() && !img.empty()) {
             if (counter > 3) {
                 counter = 0;
             } else {
@@ -187,12 +189,14 @@ void Controller::gimbal_process() {
         // cv::waitKey(10);
 
         if (fps.Count()) {
-            RCLCPP_INFO(get_logger(), "Game Stage: %s , Fps:%d", get_stage(*stage_).c_str(), fps.GetFPS());
+            RCLCPP_INFO(
+                get_logger(), "Game Stage: %s , Fps:%d", get_stage(*stage_).c_str(), fps.GetFPS());
         }
     } // while rclcpp::ok end
 }
 
-void Controller::communicate(const rmcs_msgs::ArmorID& id, const rmcs_description::OdomImu::Position& pos) {
+void Controller::communicate(
+    const rmcs_msgs::ArmorID& id, const rmcs_description::OdomImu::Position& pos) {
     Eigen::Vector2d plate_pos{pos->x(), pos->y()};
     Eigen::Vector2d zero{0, 0};
     switch (id) {
@@ -253,8 +257,8 @@ void Controller::communicate(const rmcs_msgs::ArmorID& id, const rmcs_descriptio
     default: break;
     }
     RCLCPP_INFO(
-        get_logger(), "Detected Armor %hu=[%f,%f,%f]", static_cast<uint16_t>(id), pos->x(), pos->y(),
-        pos->z());
+        get_logger(), "Detected Armor %hu=[%f,%f,%f]", static_cast<uint16_t>(id), pos->x(),
+        pos->y(), pos->z());
 }
 
 template <typename Link>
@@ -296,8 +300,8 @@ void Controller::omni_perception_process(const std::string& device) {
         std::map<rmcs_msgs::ArmorID, typename Link::Position> targets_map;
 
         for (auto& armor : armors) {
-            auto pnp_result =
-                ArmorPnPSolver::Solve(armor, omni_fx, omni_fy, omni_cx, omni_cy, omni_k1, omni_k2, omni_k3);
+            auto pnp_result = ArmorPnPSolver::Solve(
+                armor, omni_fx, omni_fy, omni_cx, omni_cy, omni_k1, omni_k2, omni_k3);
             typename Link::Position pos{
                 pnp_result.pose.position.x, pnp_result.pose.position.y, pnp_result.pose.position.z};
             RCLCPP_INFO(
@@ -315,199 +319,31 @@ void Controller::omni_perception_process(const std::string& device) {
 
 void Controller::update() {
     if (*update_count_ == 0) {
-        if (!debug_mode_ && !robot_msg_.ready()) {
-            RCLCPP_WARN(get_logger(), "Robot ID is unknown. Waiting for robot ID...");
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            return;
+        auto video = cv::VideoCapture("/mnt/records/sample.avi");
+
+        if (!video.isOpened()) {
+            RCLCPP_WARN(get_logger(), "Failed to open video!");
         }
-        if ((debug_mode_ && debug_robot_id_ == (uint16_t)rmcs_msgs::ArmorID::Sentry)
-            || (!debug_mode_ && robot_msg_->id() == rmcs_msgs::ArmorID::Sentry)) {
-            RCLCPP_INFO(get_logger(), "Reading omni-direction perception parameters");
-            try {
-                omni_exposure_ = get_parameter("omni_exposure").as_double();
-                omni_cx        = get_parameter("omni_cx").as_double();
-                omni_cy        = get_parameter("omni_cy").as_double();
-                omni_fx        = get_parameter("omni_fx").as_double();
-                omni_fy        = get_parameter("omni_fy").as_double();
-                omni_k1        = get_parameter("omni_k1").as_double();
-                omni_k2        = get_parameter("omni_k2").as_double();
-                omni_k3        = get_parameter("omni_k3").as_double();
+        auto package_share_directory =
+            ament_index_cpp::get_package_share_directory("rmcs_auto_aim");
 
-            } catch (rclcpp::exceptions::InvalidParametersException& e) {
-                RCLCPP_WARN(get_logger(), "Failed to read parameter: %s", e.what());
-            }
+        auto buff_identifier = BuffIdentifier(package_share_directory + buff_model_path_);
+
+        cv::Mat img;
+
+        video.read(img);
+
+        auto save_video = cv::VideoWriter(
+            "/mnt/records/sample_saved.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30,
+            img.size(), true);
+        while (video.read(img)) {
+            RCLCPP_INFO(get_logger(), "Reading");
+            buff_identifier.Identify(img);
+            // cv::imshow("img", img);
+            // cv::waitKey(1);
+            save_video.write(img);
         }
-
-        /******************************
-            Auto Gimbal Aiming System
-         ******************************/
-        threads_.emplace_back([this]() {
-            size_t attempt = 0;
-            while (true) {
-                if (!debug_mode_ && robot_msg_->id() == rmcs_msgs::ArmorID::Unknown) {
-                    RCLCPP_WARN(get_logger(), "Robot ID is unknown. Waiting for robot ID...");
-                    // continue;
-                } else {
-                    try {
-                        gimbal_process();
-                    } catch (std::exception& e) {
-                        attempt++;
-                        if (attempt < 10) {
-                            RCLCPP_FATAL(get_logger(), "Gimbal Error: %s", e.what());
-                        } else if (attempt == 10) {
-                            RCLCPP_WARN(get_logger(), "Too many error. Diabled log...");
-                        }
-                    }
-                }
-                std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-            }
-        });
-
-        /********************
-            Image Recorder
-         *******************/
-        if (record_mode_) {
-            threads_.emplace_back([this]() {
-                if (recorder_.is_opened()) {
-                    RCLCPP_INFO(get_logger(), "RECORDING %s...", recorder_.get_filename().c_str());
-                }
-                size_t t = 0;
-                while (rclcpp::ok()) {
-                    if (!recorder_.is_opened()) {
-                        continue;
-                    }
-                    std::unique_lock<std::mutex> lock(img_mtx_);
-                    img_cv_.wait(lock, [this] { return !image_queue_.empty(); });
-                    std::shared_ptr<cv::Mat> imgPtr = image_queue_.front().second;
-                    auto timestamp                  = image_queue_.front().first;
-                    image_queue_.pop();
-                    lock.unlock();
-
-                    cv::Mat img = *imgPtr;
-                    recorder_.record_frame(img);
-                    if (raw_img_pub_mode_ && t++ >= 3) {
-                        t = 0;
-                        debugger_.publish_raw_image(img, timestamp);
-                    }
-                }
-            });
-        }
-
-        /*****************************************
-            Omni Direction Perception System
-         *****************************************/
-        if ((debug_mode_ && debug_robot_id_ == (uint16_t)rmcs_msgs::ArmorID::Sentry)
-            || (!debug_mode_ && robot_msg_->id() == rmcs_msgs::ArmorID::Sentry)) {
-            threads_.emplace_back([this]() {
-                size_t attempt = 0;
-                while (true) {
-                    try {
-                        omni_perception_process<rmcs_description::OmniLinkLeftFront>("/dev/leftfront");
-                    } catch (std::exception& e) {
-                        attempt++;
-                        if (attempt < 10) {
-                            RCLCPP_FATAL(get_logger(), "Omni direction perception<Left Front>: %s", e.what());
-                        } else if (attempt == 10) {
-                            RCLCPP_WARN(get_logger(), "Too many error. Diabled log...");
-                        }
-                    }
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-                }
-            });
-            threads_.emplace_back([this]() {
-                size_t attempt = 0;
-                while (true) {
-                    try {
-                        omni_perception_process<rmcs_description::OmniLinkRightFront>("/dev/rightfront");
-                    } catch (std::exception& e) {
-                        attempt++;
-                        if (attempt < 10) {
-                            RCLCPP_FATAL(
-                                get_logger(), "Omni direction perception<Right Front>: %s", e.what());
-                        } else if (attempt == 10) {
-                            RCLCPP_WARN(get_logger(), "Too many error. Diabled log...");
-                        }
-                    }
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-                }
-            });
-            threads_.emplace_back([this]() {
-                size_t attempt = 0;
-                while (true) {
-                    try {
-                        omni_perception_process<rmcs_description::OmniLinkLeft>("/dev/left");
-                    } catch (std::exception& e) {
-                        attempt++;
-                        if (attempt < 10) {
-                            RCLCPP_FATAL(get_logger(), "Omni direction perception<Left>: %s", e.what());
-                        } else if (attempt == 10) {
-                            RCLCPP_WARN(get_logger(), "Too many error. Diabled log...");
-                        }
-                    }
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-                }
-            });
-            threads_.emplace_back([this]() {
-                size_t attempt = 0;
-                while (true) {
-                    try {
-                        omni_perception_process<rmcs_description::OmniLinkRight>("/dev/right");
-                    } catch (std::exception& e) {
-                        attempt++;
-                        if (attempt < 10) {
-                            RCLCPP_FATAL(get_logger(), "Omni direction perception<Right>: %s", e.what());
-                        } else if (attempt == 10) {
-                            RCLCPP_WARN(get_logger(), "Too many error. Diabled log...");
-                        }
-                    }
-                    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-                }
-            });
-        }
-    }
-
-    auto local_target = target_.release();
-    if (!local_target) {
-        // *control_direction_ = Eigen::Vector3d::Zero();
-        return;
-    }
-    using namespace std::chrono_literals;
-    auto diff = std::chrono::steady_clock::now() - timestamp_;
-    if (diff > std::chrono::milliseconds(gimbal_predict_duration_)) {
-        *control_direction_ = Eigen::Vector3d::Zero();
-        return;
-    }
-
-    auto offset =
-        fast_tf::cast<rmcs_description::OdomImu>(rmcs_description::MuzzleLink::Position{0, 0, 0}, *tf_);
-
-    double fly_time = 0;
-    for (int i = 5; i-- > 0;) {
-        auto pos =
-            local_target->Predict(static_cast<std::chrono::duration<double>>(diff).count() + fly_time + 0.05);
-        auto aiming_direction = *trajectory_.GetShotVector(
-            {pos->x() - offset->x(), pos->y() - offset->y(), pos->z() - offset->z()}, 27.0, fly_time);
-
-        auto yaw_axis = fast_tf::cast<rmcs_description::PitchLink>(
-                            rmcs_description::OdomImu::DirectionVector(0, 0, 1), *tf_)
-                            ->normalized();
-        auto pitch_axis = fast_tf::cast<rmcs_description::PitchLink>(
-                              rmcs_description::OdomImu::DirectionVector(0, 1, 0), *tf_)
-                              ->normalized();
-        auto delta_yaw   = Eigen::AngleAxisd{yaw_error_, yaw_axis};
-        auto delta_pitch = Eigen::AngleAxisd{pitch_error_, pitch_axis};
-        aiming_direction = delta_pitch * (delta_yaw * (aiming_direction));
-
-        if (i == 0) {
-            *control_direction_ = aiming_direction;
-            break;
-        }
-    }
-
-    if (target_updated_.load()) {
-        delete local_target;
-        target_updated_.store(false);
-    } else {
-        target_.reset(local_target);
+        save_video.release();
+        RCLCPP_INFO(get_logger(), "Done");
     }
 }
